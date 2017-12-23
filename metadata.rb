209f657da6b9bf1f4c576dca41303fcbf72e051c
CaptainFluffyToes <DJKhan85@gmail.com>
2017-12-22 22:10:56 -0500

    Fixed spacing in metadata file.

--- metadata.rb
+++ metadata.rb
@@ -19,15 +19,15 @@ chef_version '>= 12.1' if respond_to?(:chef_version)
 #
 # source_url 'https://github.com/CaptainFluffyToes/docker_configuration'
 
-#Changelog
-#[10/13/2017] - 0.1.1 - Created setup.rb.  Added gitignore file for .chef.  Added docker cookbook from supermarket
-#[10/14/2017] - 0.1.2 - Removed test images from the setup.rb
-#[10/16/2017] - 0.1.3 - Added packages for docker installation
-#[10/16/2017] - 0.1.4 - Updated berksfile source to include chef server
-#[10/16/2017] - 0.1.5 - Made chef-apt-docker run first
-#[10/18/2017] - 0.1.6 - Created loops and conditional checks for scripts
-#[10/25/2017] - 0.1.7 - Added tests for debian and rhel distros
-#[12/22/2017] - 0.1.8 - Added dynamic renaming of machines so that -docker.solsys.com is checked and then the machine is renamed if not. 
+# Changelog
+# [10/13/2017] - 0.1.1 - Created setup.rb.  Added gitignore file for .chef.  Added docker cookbook from supermarket
+# [10/14/2017] - 0.1.2 - Removed test images from the setup.rb
+# [10/16/2017] - 0.1.3 - Added packages for docker installation
+# [10/16/2017] - 0.1.4 - Updated berksfile source to include chef server
+# [10/16/2017] - 0.1.5 - Made chef-apt-docker run first
+# [10/18/2017] - 0.1.6 - Created loops and conditional checks for scripts
+# [10/25/2017] - 0.1.7 - Added tests for debian and rhel distros
+# [12/22/2017] - 0.1.8 - Added dynamic renaming of machines so that -docker.solsys.com is checked and then the machine is renamed if not. 
 
 depends 'docker'
 depends 'chef-apt-docker'
