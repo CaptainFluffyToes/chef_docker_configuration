b9bbf0624b8fb1dd457fb58b096a803de971a700
CaptainFluffyToes <DJKhan85@gmail.com>
2017-12-28 17:48:13 -0500

    updated metadata

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'djkhan85@gmail.com'
 license 'All Rights Reserved'
 description 'Installs/Configures docker_configuration'
 long_description 'Installs/Configures docker_configuration'
-version '0.1.14'
+version '0.1.15'
 chef_version '>= 12.1' if respond_to?(:chef_version)
 
 # The `issues_url` points to the location where issues for this cookbook are
